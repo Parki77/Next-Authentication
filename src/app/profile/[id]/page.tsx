9e95c363749e67a047bbc57c9e580fd69938bf64
Partha Basak <basakpartha595@gmail.com>
2024-11-18 01:58:57 +0530

    all good

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,3 +1,6 @@
+// pages/profile/[id].tsx
+
+// import { useRouter } from 'next/router';
 
 interface UserProfileProps {
   params: {
@@ -5,15 +8,28 @@ interface UserProfileProps {
   };
 }
 
-export default function Userprofile({ params }: UserProfileProps){
+export default function UserProfile({ params }: UserProfileProps) {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1>
-        profile
-      </h1>
+      <h1>Profile</h1>
       <hr />
       <p className="text-4xl"></p>
       <span className="p-2 rounded bg-orange-600">{params.id}</span>
     </div>
-  )
+  );
+}
+
+// Optionally, if you're using `getServerSideProps` or `getStaticProps`
+// to fetch `params` you would need to define those as well, like so:
+
+export async function getServerSideProps(context: { params: { id: string } }) {
+  const { id } = context.params;
+  // You can now fetch data based on `id`
+  return {
+    props: {
+      params: {
+        id,
+      },
+    },
+  };
 }
